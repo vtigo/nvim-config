1d3667220e9cf93932bf5460616174c31844991f
tigo <rf.tigas@gmail.com>
2025-07-22 20:32:23 -0300

    add new theme stuff

--- init.lua
+++ init.lua
@@ -10,17 +10,34 @@ autocmd('LspAttach', {
         -- Navigation
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { buffer = e.buf, desc = "Go to definition" })
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = e.buf, desc = "Show documentation" })
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, { buffer = e.buf, desc = "Search workspace symbols" })
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end,
+            { buffer = e.buf, desc = "Search workspace symbols" })
         -- Diagnostics
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, { buffer = e.buf, desc = "Show diagnostics" })
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end,
+            { buffer = e.buf, desc = "Show diagnostics" })
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, { buffer = e.buf, desc = "Next diagnostic" })
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, { buffer = e.buf, desc = "Previous diagnostic" })
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end,
+            { buffer = e.buf, desc = "Previous diagnostic" })
         -- Code actions
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, { buffer = e.buf, desc = "Code actions" })
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, { buffer = e.buf, desc = "Find references" })
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, { buffer = e.buf, desc = "Rename symbol" })
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,
+            { buffer = e.buf, desc = "Code actions" })
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
+            { buffer = e.buf, desc = "Find references" })
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end,
+            { buffer = e.buf, desc = "Rename symbol" })
         -- Help
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = e.buf, desc = "Show function signature" })
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end,
+            { buffer = e.buf, desc = "Show function signature" })
+    end
+})
+
+-- Set tab spacing to 2 for TypeScript and JavaScript files
+autocmd('FileType', {
+    pattern = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'jsx', 'tsx' },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
     end
 })
 
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,42 +1,49 @@
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-  },
-  {
-      "ribru17/bamboo.nvim",
-      name = "bamboo",
-      priority = 1000,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        integrations = {
-            cmp = true,
-            gitsigns = true,
-            nvimtree = true,
-            telescope = true,
-            treesitter = true,
-            which_key = true,
-        }
-    })
-    end
-  },
-  {
-      "neanias/everforest-nvim",
-      name = "everforest",
-      priority = 1000,
-      config = function()
-          require("everforest").setup({
-              transparent_background = true
-          })
-          vim.cmd.colorscheme("everforest")
-      end
-  }
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+    },
+    {
+        "ribru17/bamboo.nvim",
+        name = "bamboo",
+        priority = 1000,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    telescope = true,
+                    treesitter = true,
+                    which_key = true,
+                }
+            })
+        end
+    },
+    {
+        "neanias/everforest-nvim",
+        name = "everforest",
+        priority = 1000,
+        config = function()
+            require("everforest").setup({
+                transparent_background_level = 1,
+            })
+            vim.cmd.colorscheme("everforest")
+        end
+    },
+    {
+        "xero/miasma.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- vim.cmd("colorscheme miasma")
+        end,
+    }
 }
-
